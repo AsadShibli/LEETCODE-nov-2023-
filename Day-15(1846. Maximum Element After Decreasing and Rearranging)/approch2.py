c33fe04fb0fa5578bde3a67bc3498c134da8673d
MD. Asadullah Shibli <119102237+AsadShibli@users.noreply.github.com>
2023-11-15 20:53:59 +0600

    Delete Day-15(1846. Maximum Element After Decreasing and Rearranging) directory

--- Day-15(1846. Maximum Element After Decreasing and Rearranging)/approch2.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
-        arr.sort()
-        pre = 0
-        for i in arr:
-            pre = min(pre+1,i)
-        return pre
